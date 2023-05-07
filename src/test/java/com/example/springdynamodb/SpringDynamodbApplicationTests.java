381974670f59a9582720c1d61966f59827bcc158
Sachith Lakmal <sachithlakmal@icloud.om>
2023-05-07 13:57:46 +0400

    initial commit

---
+++ src/test/java/com/example/springdynamodb/SpringDynamodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springdynamodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDynamodbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
